efdeff9f0dd6782cb1d01849cf72c40768f74922
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-25 18:21:31 -0500

    Create Pipe.py

---
+++ llamadosalsistema/Pipe.py
@@ -0,0 +1,10 @@
+from abc import abstractclassmethod
+import os 
+
+path = 'D:/Estudio/Python' 
+
+os.chdir(path)
+  
+b = os.pipe() # Crea una tubería (método para pasar información de un proceso a otro), retorna dos fd, para leer y editar respectivamente
+
+print (b)
