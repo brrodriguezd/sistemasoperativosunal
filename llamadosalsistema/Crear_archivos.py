011b0ce6da1f1fbacd842ffddd407ab422b28dea
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-25 13:20:34 -0500

    Create Crear_archivos.py

---
+++ llamadosalsistema/Crear_archivos.py
@@ -0,0 +1,15 @@
+path = 'D:/Python'  #Directorio 
+os.chdir(path)
+
+dir_list = os.listdir(path)  #Guarda una lista de directorios, y archivos
+print("Lista de archivos y directorios antes de crear:")
+print(dir_list)
+print ()
+  
+with open('myfile.txt', 'w') as fp: #Crea el archivo (w = solo escritura, w+ escritura y lectura, a para concatenar, a+ para concatenar y leer) 
+    pass
+    fp.write("New file created") #Escribe en el archivo
+
+dir_list = os.listdir(path) #Actualiza la lista de directorios
+print("Lista de archivos y directorios despues de crear:")
+print(dir_list)
