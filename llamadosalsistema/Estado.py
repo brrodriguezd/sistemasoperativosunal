6d36715475e3719b497e4c6fe20bebcbd18893ee
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-25 14:57:25 -0500

    Create Estado.py

---
+++ llamadosalsistema/Estado.py
@@ -0,0 +1,14 @@
+import os 
+
+path = 'D:/Estudio/Python' 
+os.chdir(path)
+  
+with open('myfile.txt', 'w') as fp:
+    pass
+    fp.write("Documento")
+
+path_archivo = 'D:/Estudio/Python/myfile.txt' 
+status = os.stat(path_archivo) #Guarda el estado
+
+print(status)             #Muestra todas las estadísticas 
+print(status.st_size)     #Muestra una estadística específica
