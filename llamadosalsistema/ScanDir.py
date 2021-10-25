2f4d347aa8f2b6e4c676509a35d4b7f92d4af1c2
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-25 11:54:11 -0500

    Update ScanDir.py

--- llamadosalsistema/ScanDir.py
+++ llamadosalsistema/ScanDir.py
@@ -1,3 +1,4 @@
+a = 'C:/Users'
 with os.scandir(a) as it: #scandir(path) regresa un iterador 
     for entry in it: 
         if entry.is_dir() and entry.name.startswith('D'):			#el método is_dir() regresa un valor booleano para determinar si entry es un directorio, mientras el método name.startswith('D') revisa que el directorio empiece con D 
