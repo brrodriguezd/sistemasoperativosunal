5c3041b84b8fc1fadfae525518a62a61751b3391
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-25 15:31:39 -0500

    Create Symlink.py

---
+++ llamadosalsistema/Symlink.py
@@ -0,0 +1,14 @@
+import os 
+
+path = 'D:/Estudio/Python' 
+os.chdir(path)
+  
+#Requiere privilegios para funcionar
+with open('src', 'w') as fp: #Crea el archivo para crear el symlink
+    pass
+    fp.write("Destino") 
+
+src = 'D:/Estudio/Python/source'     
+dst = 'D:/Estudio/Python/dest'     #Dirección del symlink que apunta a src
+
+os.symlink(src, dst)
