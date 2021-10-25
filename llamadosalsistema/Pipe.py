17cc6289b8fbecf7e6ede58b44c2190e1f98feeb
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-25 18:32:03 -0500

    Update Pipe.py

--- llamadosalsistema/Pipe.py
+++ llamadosalsistema/Pipe.py
@@ -5,6 +5,6 @@ path = 'D:/Estudio/Python'
 
 os.chdir(path)
   
-b = os.pipe() # Crea una tubería (método para pasar información de un proceso a otro), retorna dos fd, para leer y editar respectivamente
+b = os.pipe() # Crea una tubería (método para pasar información de un proceso a otro), retorna una tupla con dos fd, para leer y editar respectivamente
 
 print (b)
