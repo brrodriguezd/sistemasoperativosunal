5fe3d18176e94c81f9930360da6bd70c55db5d8f
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-27 17:11:22 -0500

    Update Async.py

--- procesos/Async.py
+++ procesos/Async.py
@@ -2,43 +2,67 @@ from abc import abstractclassmethod
 import asyncio
 from asyncio.tasks import sleep
 import random
+import os
+
+path = 'D:/Estudio/Python'  												#Directorio 
+os.chdir(path)
+  
+with open('vehiculos.txt', 'w') as fp:  									#Crea el archivo
+    pass
 
 async def Semaforo(u_input):     
     for i in range (u_input):
+        with open('vehiculos.txt', 'a') as fp:         						#añade información al archivo
+            pass
+            fp.write("ciclo 1\nsemáforo vertical\n") 
         print ("Inicia el semáforo vertical (ciclo ", i+1, ")")       
-        await S_vertical()                #Espera a que termina el semáforo vertical
-        await asyncio.sleep(6)            #Cuenta el ciclo del semáforo en rojo
-        print ("Inicia el semáforo horizontal")
+        await S_vertical()                									#Espera a que termina el semáforo vertical
+        print ("\nInicia el semáforo horizontal")
         print ("\nAmarillo")
-        await asyncio.sleep(3)            #Cuenta el ciclo de amarillo
+        await asyncio.sleep(3)            									#Cuenta el ciclo de amarillo, del semáforo horizontal
+        with open('vehiculos.txt', 'a') as fp: 
+            pass
+            fp.write("\nsemáforo horizontal\n")
         await S_horizontal()
+    with open('vehiculos.txt', 'r') as fp:          						#Lee el archivo con la información de los vehículos que pasarón
+        pass
+        print(fp.read())
 
 async def carros():
     b = 0
-    while b < 7:                          #Cuenta los segundos que tarda. El ciclo de verde tarda 6 segundos, por lo que hay una probabilidad de que alguien pase en amarillo mas no en rojo, añadido para más realismo xD
-        a = random.randint(1,3)
+    while b < 7:                          									#Cuenta los segundos que tarda. El ciclo de verde tarda 6 segundos,
+        a = random.randint(1,3)												#por lo que hay una probabilidad de que alguien pase en amarillo mas no en rojo, añadido para más realismo xD
         if a == 1:
             print ("paso un carro")
+            with open('vehiculos.txt', 'a') as fp:    						#añade información al archivo
+                pass
+                fp.write("  carro\n ")
             b += 1
-            await asyncio.sleep(1)        #Espera a que pase el vehículo
+            await asyncio.sleep(1)        									#Espera a que pase el vehículo
         elif a == 2:
             print ("paso un bus")
+            with open('vehiculos.txt', 'a') as fp:       					#añade información al archivo
+                pass
+                fp.write("  bus\n ")
             b += 2
             await asyncio.sleep(2)
         elif a == 1:
             print ("paso un camión")
+            with open('vehiculos.txt', 'a') as fp:      					#añade información al archivo
+                pass
+                fp.write("  camión\n ")
             b += 3
             await asyncio.sleep(3)
 
 async def S_vertical():     
     print("\nVerde \n")
-    task = asyncio.create_task(carros())  #Hace una tarea en simultaneo, que cuenta carros
-    await asyncio.sleep(6)                #Espera a que cuente 7 para cambiar 
+    task = asyncio.create_task(carros())  									#Hace una tarea en simultaneo, que cuenta carros
+    await asyncio.sleep(6)                									#Espera a que cuente 7 para cambiar 
     print ("\nAmarillo \n") 
-    await asyncio.sleep(3)                #Espera a que cuente 3 para cambiar
+    await asyncio.sleep(3)                									#Espera a que cuente 3 para cambiar
     print ("Rojo \n")
 
-async def S_horizontal():                 #Igual que la vertical
+async def S_horizontal():                 									#Igual que la vertical
     print("\nVerde \n")
     task = asyncio.create_task(carros())   
     await asyncio.sleep(6)
@@ -47,7 +71,7 @@ async def S_horizontal():                 #Igual que la vertical
     print ("Rojo \n") 
 
 def U_input():
-    print("Cúantos ciclos de semáforo: ")  #Pide al usuario el número de ciclos y maneja errores
+    print("Cúantos ciclos de semáforo: ")  									#Pide al usuario el número de ciclos y maneja errores
     u_input = input()
     try:
         u_input = int(u_input)
@@ -60,6 +84,6 @@ def U_input():
             print("Ingrese un número entero positivo\n")
             U_input()
         else:
-            asyncio.run(Semaforo(u_input))    #Llama la función async desde una no async
+            asyncio.run(Semaforo(u_input))    								#Llama la función async desde una no async
 
 U_input()
