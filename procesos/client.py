edafbae6bcdf1e3d3cac3c3632c04ba645ba28e4
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-10-27 23:08:31 -0500

    Update client.py

--- procesos/client.py
+++ procesos/client.py
@@ -1,9 +1,9 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '127.0.0.1'  
+PORT = 65432        
 
-def respuesta():
+def respuesta():            #Datos codificados 
     a = input()
     if (a == ''):
         a = 'a'
@@ -14,12 +14,12 @@ def respuesta():
     return a
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    #creates a socket
     s.connect((HOST, PORT))                                      #connects to the server   
-    while True:
-        data = s.recv(1024)   
-        if not data:
+    while True:                                                    #bucle para las multiples respuestas
+        data = s.recv(1024)                                         #recibe datos
+        if not data:                                                #si no recibe para el programa
             break                             
-        print(data.decode('utf-8'))                                   #prints received data
-        answer = respuesta()
-        s.sendall(answer)                                        #sends message 
+        print(data.decode('utf-8'))                                  #decodifica los mensajes
+        answer = respuesta()                                         #recibe el input del usuario 
+        s.sendall(answer)                                        #Manda el mensaje
     s.close()     
     
