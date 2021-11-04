337757b8e45fbd9fe34763b4a02623dadb511522
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-11-04 15:38:52 -0500

    Update Punto1.py

--- Parcial1/Punto1.py
+++ Parcial1/Punto1.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import os
-
-os.chdir('D:/Estudio/Python')
+#Directorio de trabajo
+os.chdir('D:/Python')
 
 funciones = "Funciones: \nAdministrar los recursos del hardware\nActuar como interfaz entre el usuario y la máquina\nRelacionar los dispositivos\nManejar las comunicaciones de redes\nFacilitar el uso del dispositivo"
+#Separa por cambios de linea
+b = funciones.splitlines()
+#Crea o sobreescribe un archivo con txt con las funciones
 with open('5FuncOS.txt','w') as fp:
     pass
     fp.write(funciones)
 
+#Crea un data frame con las funciones
 df = pd.DataFrame(
     {
-        "Funciones": ["Administrar los recursos del hardware", "Actuar como interfaz entre el usuario y la máquina", "Relacionar los dispositivos", "Manejar las comunicaciones de redes", "Facilitar el uso del dispositivo"],
+        b[0]: b[1:6],
     }
 )
-
-df
+#Importa el data frame a un archivo de excel
+df.to_excel("FuncionesSO.xlsx", sheet_name="Funciones")
