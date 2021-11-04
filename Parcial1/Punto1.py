6baab538efd0243aa3122c20291dbcec8030d0ea
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-11-04 14:58:19 -0500

    Update Punto1.py

--- Parcial1/Punto1.py
+++ Parcial1/Punto1.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import os
 
+os.chdir('D:/Estudio/Python')
+
+funciones = "Funciones: \nAdministrar los recursos del hardware\nActuar como interfaz entre el usuario y la máquina\nRelacionar los dispositivos\nManejar las comunicaciones de redes\nFacilitar el uso del dispositivo"
+with open('5FuncOS.txt','w') as fp:
+    pass
+    fp.write(funciones)
+
+df = pd.DataFrame(
+    {
+        "Funciones": ["Administrar los recursos del hardware", "Actuar como interfaz entre el usuario y la máquina", "Relacionar los dispositivos", "Manejar las comunicaciones de redes", "Facilitar el uso del dispositivo"],
+    }
+)
+
+df
