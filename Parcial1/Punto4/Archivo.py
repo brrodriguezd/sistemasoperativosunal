d6f43ee013e4a721b3c9e7a633ba130f5b486e9f
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-11-05 18:23:50 -0500

    Update Archivo.py

--- Parcial1/Punto4/Archivo.py
+++ Parcial1/Punto4/Archivo.py
@@ -7,30 +7,34 @@ PORT = 65432
 
 dir = '/home/camilo/Documentos/Python'
 os.chdir(dir)
-
+#Crea el archivo que va a pasar al cliente
 with open('archivo.txt','w') as fp:
     pass
     fp.write("Crear archivo")
-	
+
+#Lee la dirección del directorio y scanea archivos .txt
 async def codificar():
     a = ''
-    with os.scandir(dir) as it: #scandir(path) regresa un iterador 
+    with os.scandir(dir) as it:
         for entry in it: 
-            if entry.is_file() and entry.name.endswith('.txt'):			#el método is_dir() regresa un valor booleano para determinar si entry es un directorio, mientras el método name.startswith('D') revisa que el directorio empiece con D 
+            if entry.is_file() and entry.name.endswith('.txt'):
                 with open(entry,'r') as fp:
                     pass
+										#Añade el nombre de los archivos y el contenido separado por ###
                     a = a + entry.name + '###' + fp.read()+ '###'
-    a = bytes(a,'utf-8')
+    #Convierte a bytes con encriptacion utf-8
+	a = bytes(a,'utf-8')
     return a
 
 async def enviar():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creates a socket
-    s.connect((HOST, PORT))                                      #connects to the server   
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
+    s.connect((HOST, PORT))   
     a = await codificar()
     s.send(a)                          
-    print(a)                                   #prints received data
+    print(a)                                   
     while True:
         data = s.recv(1024)
+		#Espera a recibir f para acabar el programa
         if data == (b'f'):
             break
     s.close()
