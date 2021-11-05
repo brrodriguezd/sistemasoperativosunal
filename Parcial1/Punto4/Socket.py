20bc06b67d0222c0bb1841f01c54d97ffc8a8360
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-11-05 18:29:19 -0500

    Update Socket.py

--- Parcial1/Punto4/Socket.py
+++ Parcial1/Punto4/Socket.py
@@ -12,27 +12,35 @@ async def descifrar(a):
     a = a.decode('utf-8')
     return a
 
+#Manda unos bytes a una conexión
 async def mandar(conn, a):
     conn[0].send(a)
 
 async def main():
     print("Esperando conexión")
+    #Crea el socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))            
-    s.listen(5)                      
+    s.listen(5)         
+    #Se conecta al archivo
     connection = s.accept() 
-    print('Connected by', connection[1])             
+    print('Connected by', connection[1])   
+    #Recibe el archivo
     data = connection[0].recv(1024)
     tarea = asyncio.create_task(descifrar(data))
+    #Se conecta al cliente
     connection2 = s.accept() 
     print('Connected by', connection2[1])             
     a = connection2[0].recv(1024)
+    #envia el archivo al cliente
     enviar = await mandar(connection2, data)
     a = connection2[0].recv(1024)
+    #empieza dos corutinas para acabar las conexiones
     terminar = asyncio.create_task(mandar(connection, (b'f')))
     task = asyncio.create_task(mandar(connection2, (b'f')))
     while True:
         try:
+            #maneja errores 
             a = terminar.result()
             a = task.result()
             data = tarea.result()
@@ -40,6 +48,7 @@ async def main():
             await asyncio.sleep(0)
         else:
             break
+    #Termina la conexion y cierra todo
     print("Conexión terminada")
     print(data)
     connection[0].close()
