b388581ba838e32b7e43abb7c308778e58f999a5
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-11-06 12:29:42 -0500

    Update Punto2.py

--- Parcial1/Punto2.py
+++ Parcial1/Punto2.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 16:14:10 2021
+
+@author: camil
+"""
+
 import socket
 import asyncio
+from os import chdir
 #HTTPS
 import ssl
+
+chdir ('D:/Estudio/Python')
 
 HOST = 'www.buda.com'
 PORT = 443
@@ -9,14 +19,12 @@ context = ssl.create_default_context()
 
 with open('buda.txt', 'w') as fp:
     pass
-a = ("GET /api/v2/markets HTTP/1.0\r\nHost: www.buda.com\r\n\r\n")
+a = ("GET /api/v2/markets HTTP/1.1\r\nHost: www.buda.com\r\n\r\n")
 a = a.encode()
 
-#TLS SSL para https
-
-with socket.socket(socket.AF_INET ,socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     print('connecting')
-    with context.wrap_socket(s,server_hostname=HOST) as ssock:
+    with context.wrap_socket(s, server_hostname=HOST) as ssock:
         ssock.connect((HOST,PORT))
         print('connected')
         ssock.send(a)
@@ -29,3 +37,4 @@ with socket.socket(socket.AF_INET ,socket.SOCK_STREAM) as s:
                 fp.write(str(data))
             if not data:
                 break
+        print ("conexión terminada")
