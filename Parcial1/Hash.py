242d316b10f1def53301b3860637f9be18d90a5c
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2021-11-09 11:21:23 -0500

    Add files via upload

--- Parcial1/Hash.py
+++ Parcial1/Hash.py
@@ -1,24 +1,7 @@
 import os
 import hashlib
 
-os.chdir ('/home/camilo/Documentos/Python/Parcial1')
-
-a = list()
-names = list()
-for entry in os.scandir():
-    if (entry.name.endswith('.py') or entry.name.endswith('.txt')) and not entry.name.startswith('hash '):
-        names.append(entry.name)
-        with open(entry.name, 'r') as fp:
-            a.append(fp.read())
-
-for i in range (len(a)):
-    m = hashlib.sha1()
-    m.update(bytes(a[i], 'utf-8'))
-    name = 'hash sha1 '+ str(names[i].split('.')[0])+':' + '.txt'
-    with open(name, 'w') as fp:
-        fp.write(str(m.digest()))
-
-os.chdir ('/home/camilo/Documentos/Python/Parcial1/Punto4')
+os.chdir ('C:/Users/camil/Documents/Parcial1/Parcial1')
 
 a = list()
 names = list()
@@ -33,9 +16,9 @@ for i in range (len(a)):
     m.update(bytes(a[i], 'utf-8'))
     name = 'hash sha1 '+ str(names[i].split('.')[0]) + '.txt'
     with open(name, 'w') as fp:
-        fp.write(str(m.digest()))
+        fp.write(str(m.hexdigest()))
 
-os.chdir ('/home/camilo/Documentos/Python/Parcial1/Buda')
+os.chdir ('C:/Users/camil/Documents/Parcial1/Parcial1/Punto4')
 
 a = list()
 names = list()
@@ -50,4 +33,21 @@ for i in range (len(a)):
     m.update(bytes(a[i], 'utf-8'))
     name = 'hash sha1 '+ str(names[i].split('.')[0]) + '.txt'
     with open(name, 'w') as fp:
-        fp.write(str(m.digest()))
+        fp.write(str(m.hexdigest()))
+
+os.chdir ('C:/Users/camil/Documents/Parcial1/Parcial1/Buda')
+
+a = list()
+names = list()
+for entry in os.scandir():
+    if (entry.name.endswith('.py') or entry.name.endswith('.txt')) and not entry.name.startswith('hash '):
+        names.append(entry.name)
+        with open(entry.name, 'r') as fp:
+            a.append(fp.read())
+
+for i in range (len(a)):
+    m = hashlib.sha1()
+    m.update(bytes(a[i], 'utf-8'))
+    name = 'hash sha1 '+ str(names[i].split('.')[0]) + '.txt'
+    with open(name, 'w') as fp:
+        fp.write(str(m.hexdigest()))
