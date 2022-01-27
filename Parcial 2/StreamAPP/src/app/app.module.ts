c88984157a5cafa4f48832e4c5c130960143d250
brrodriguezd <92541276+brrodriguezd@users.noreply.github.com>
2022-01-27 01:31:07 -0500

    Add files via upload

--- Parcial 2/StreamAPP/src/app/app.module.ts
+++ Parcial 2/StreamAPP/src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiComponent } from './api/api.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ApiComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [
-    HttpClient,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
